6fa5792d1e2d037d1c23f5724807ac3ee2fd0b13
Andrzej Kądzielawa <apkadzielawa@gmail.com>
2025-11-18 09:18:57 +0100

    New lab class: on-fly compilation  - 003/run/animation.py  - 003/run/cprototype.py  - 003/run/particles.py  - 003/solver/solver.c

---
+++ 003/run/animation.py
@@ -0,0 +1,112 @@
+# === IMPORTS ===
+# Standard library imports
+import itertools as it
+
+# Numpy (https://numpy.org/)
+# and ctypes (https://docs.python.org/3/library/ctypes.html)
+import numpy as np
+
+# Matplotlib (https://matplotlib.org/) 
+# imports for plotting and animation
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib.animation as animation
+
+
+class Animation2D:
+    def __init__(self,vector_factory=None, c_arr=None,
+                 next_step=None,positions=None, velocities=None,
+                 dt=0.01,NUMBER_OF_PARTICLES=1):
+        self.data = np.zeros((NUMBER_OF_PARTICLES, 2))
+
+        self.vector = vector_factory
+        self.c_arr = c_arr
+        self.next_step = next_step
+        self.set_positions(positions)
+        self.set_velocities(velocities)
+        self.colours = it.cycle(mcolors.TABLEAU_COLORS)
+        self.dt = dt
+        self.NUMBER_OF_PARTICLES = NUMBER_OF_PARTICLES
+        pass
+
+    def set_positions(self, positions):
+        self.positions = positions
+        for i, pos in enumerate(self.positions):
+            pos(self.data, i)  # Initialize 'self.data' with starting positions
+
+    def set_velocities(self, velocities):
+        self.velocities = velocities
+
+    def create_canvas(self,**kwargs):
+        """
+        This function sets up the Matplotlib figure and axes for the animation.
+        It initializes the plot elements that will be updated in each frame.
+        """
+        self.fig, self.ax = plt.subplots()
+        self.ax.set_aspect('equal', adjustable='box')  # Ensure x and y axes have the same scale
+        # Set plot limits and labels
+        if 'xlim' not in kwargs:
+            kwargs['xlim']=[-2.1, 2.1]
+        if 'ylim' not in kwargs:
+            kwargs['ylim']=[-2.1, 2.1]
+        if 'xlabel' not in kwargs:
+            kwargs['xlabel']='x'
+        if 'ylabel' not in kwargs:
+            kwargs['ylabel']='y'
+        self.ax.set(**kwargs)
+        if self.NUMBER_OF_PARTICLES > 1:
+            # Append first point to close the loop
+            self.lines = self.ax.plot(np.append(self.data[:, 0], self.data[0, 0]),
+                                      np.append(self.data[:, 1], self.data[0, 1]), lw=1)[0]
+        # 'points' is a scatter plot of the particles themselves
+        self.points = self.ax.scatter(self.data[:, 0], self.data[:, 1],
+                      c=[clr for clr, _ in zip(self.colours, range(self.NUMBER_OF_PARTICLES))], s=57)
+        pass
+
+    def update_frame(self, frame):
+        """
+        This function is called for each frame of the animation.
+        It calculates the new state of the simulation and updates the plot.
+        """
+        global positions,velocities
+
+        # Create empty Vector2D objects to hold the C function results
+        new_positions = [self.vector(x=0, y=0) for i in range(self.NUMBER_OF_PARTICLES)]
+        new_velocities= [self.vector(x=0, y=0) for i in range(self.NUMBER_OF_PARTICLES)]
+
+        c_positions       = self.c_arr(*self.positions)
+        c_velocities      = self.c_arr(*self.velocities)
+        c_new_positions   = self.c_arr(*new_positions)
+        c_new_velocities  = self.c_arr(*new_velocities)
+
+        # 1. Calculate the new positions and velocities
+        self.next_step(c_positions, c_velocities,
+                       c_new_positions, c_new_velocities,
+                       self.dt, self.NUMBER_OF_PARTICLES)
+        self.positions  = c_positions[:]
+        self.velocities = c_velocities[:]
+        new_positions   = c_new_positions[:]
+        new_velocities  = c_new_velocities[:]
+
+        for i,new_position in enumerate(new_positions):
+            # 2. Update the master Python lists with the new state
+            self.positions[i]  = new_position
+            self.velocities[i] = new_velocities[i]
+
+            # 3. Update the NumPy plotting array
+            new_position(self.data, i)
+
+        # --- Update Matplotlib elements ---
+        # Update the positions of the scattered points
+        self.points.set_offsets(self.data)
+
+        # Update the connecting lines (if they exist)
+        if self.NUMBER_OF_PARTICLES > 1:
+            self.lines.set_xself.data(np.append(self.data[:, 0], self.data[0, 0]))
+            self.lines.set_yself.data(np.append(self.data[:, 1], self.data[0, 1]))
+
+    def run_animation(self,frames=60,interval=30):
+        self.ani = animation.FuncAnimation(fig=self.fig, func=self.update_frame,
+                                           frames=frames , interval=interval)
+        plt.show()
+
---
+++ 003/run/particles.py
@@ -0,0 +1,50 @@
+# === IMPORTS ===
+# Standard library imports
+import os
+from sys import argv
+
+# Third party imports
+import numpy as np
+
+# Local imports
+import cprototype as cp
+import animation as anim
+
+# === CONSTANTS ===
+if len(argv) > 1:
+    try:
+        NUMBER_OF_PARTICLES = int(argv[1]) # Number of particles read from command line
+    except ValueError as e:
+        print(e)
+        NUMBER_OF_PARTICLES = np.random.randint(1,23)
+        print(f"Number of particles is now set to randomly chosen: {NUMBER_OF_PARTICLES}")
+else:
+    NUMBER_OF_PARTICLES = 1      # Number of particles in the simulation
+RADIUS              = 2.0    # Initial radius for particle placement
+dt                  = 0.01   # Timestep for the simulation
+
+# === C LIBRARY LOADING ===
+# Define the path to the compiled C library (.so file)
+# This assumes 'libsolver.so' is in a 'solve' directory one level *up*
+# from the directory containing this Python script.
+__solver_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'solver/libsolver.so'))
+_libsolver    = cp.EOMSolver(__solver_path, NUMBER_OF_PARTICLES, DIMENSIONS=2)
+
+# +== INITIAL CONDITIONS ===
+positions = [_libsolver.vector(x=RADIUS * np.cos(2 * np.pi * i / NUMBER_OF_PARTICLES),
+                               y=RADIUS * np.sin(2 * np.pi * i / NUMBER_OF_PARTICLES))
+             for i in range(NUMBER_OF_PARTICLES)]
+velocities = [_libsolver.vector(x=0, y=0) for i in range(NUMBER_OF_PARTICLES)]
+
+# === PLOTTING SETUP ===
+ani = anim.Animation2D(vector_factory=_libsolver.vector,
+                       c_arr=_libsolver.c_arr,
+                       next_step=_libsolver.next_step,
+                       positions=positions,
+                       velocities=velocities,
+                       dt=dt,
+                       NUMBER_OF_PARTICLES=NUMBER_OF_PARTICLES)
+ani.create_canvas()
+
+# === RUN ANIMATION ===
+ani.run_animation()
